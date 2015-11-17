2ce8c30fede891ceb6dc793023cda99f1a4ce8df
Boubacar Diarra <boubadiarra@gmail.com>
2015-11-18 00:36:05 +0100

    initial import

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'bootstrap';
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging()
+    .plugin('aurelia-validation', (config) => { config.useLocale('fr-FR') }); //Add this line to load the plugin
+
+  //Uncomment the line below to enable animation.
+  //aurelia.use.plugin('aurelia-animator-css');
+
+  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+  aurelia.start().then(a => a.setRoot());
+}
